28e58684eef837cd0987d49d22049189b4ac3355
Bandeena <lindseysonnamaker@gmail.com>
2021-10-04 22:41:54 -0500

    correcting for bad path

--- Python_practice.py
+++ Python_practice.py
@@ -1,4 +1,22 @@
 counties = ["Arapahoe","Denver","Jefferson"]
-# print (counties)
 
-counties[0]
+counties_dict = {
+    "Arapahoe": 422829, 
+    "Denver": 463353, 
+    "Jefferson": 432438}
+
+# if counties[1] == 'Denver':
+#     print (counties[1])
+
+# if "El Paso" in counties:
+#     print("El Paso is in the list of counties.")
+# else:
+#     print("El Paso is not the list of counties.")
+
+# for county in counties:
+#     print(county)
+
+for county in counties_dict:
+    print(county)
+
+
